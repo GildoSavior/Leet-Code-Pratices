9636d67770ba8386257fb591dc443320febd3394
GildoSavior <gildosalva.pe@gmail.com>
2025-01-13 13:38:27 +0100

    feat add TwoSum method to test

--- Two Sum_1/src/main/java/org/example/Main.java
+++ Two Sum_1/src/main/java/org/example/Main.java
@@ -4,14 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] nums = {2, 7, 11, 15};
+        twoSum(nums, 9);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i <= nums.length; i++ ) {
+            System.out.println("teste");
         }
+        return null;
     }
 }
