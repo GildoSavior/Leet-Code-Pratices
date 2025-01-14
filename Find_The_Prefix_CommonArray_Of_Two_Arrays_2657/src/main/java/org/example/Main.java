f381de1142b77fb6e0e7015acff64d9100f32c39
GildoSavior <gildosalva.pe@gmail.com>
2025-01-14 08:37:46 +0100

    feat implementing the method

--- Find_The_Prefix_CommonArray_Of_Two_Arrays_2657/src/main/java/org/example/Main.java
+++ Find_The_Prefix_CommonArray_Of_Two_Arrays_2657/src/main/java/org/example/Main.java
@@ -1,17 +1,39 @@
 package org.example;
 
+import java.util.HashSet;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] C = new int[n]; // Resultant prefix common array
+        HashSet<Integer> seenInA = new HashSet<>(); // Track elements seen in A
+        HashSet<Integer> seenInB = new HashSet<>(); // Track elements seen in B
+
+        int commonCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Add the current element of A to the set
+            if (seenInB.contains(A[i])) {
+                commonCount++;
+            }
+            seenInA.add(A[i]);
+
+            // Add the current element of B to the set
+            if (seenInA.contains(B[i])) {
+                commonCount++;
+            }
+            seenInB.add(B[i]);
+
+            // Store the common count in the result array
+            C[i] = commonCount;
+        }
+
+        return C;
+    }
+
 }
